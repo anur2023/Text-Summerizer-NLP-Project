b582290305c507c8aed927ecb47d25350fb0619d
Anuruddh <anuruddh209401@gmail.com>
2024-12-10 00:00:44 +0530

    Installation work have been done

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
-# Python module
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+REPO_NAME = "Text-Summerizer-NLP-Project"
+AUTHOR_USER_NAME = "anur2023"
+SRC_REPO = "textSummerizer"
+AUTHOR_EMAIL = "anuruddh209401@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small Python package for text summarization using NLP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        # Add dependencies here, for example:
+        # "numpy>=1.21.0",
+        # "nltk>=3.6.0",
+    ],
+)
